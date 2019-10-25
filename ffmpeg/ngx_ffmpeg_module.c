a8eace3c88cbdcc6c36cc23531db54aad7a4064a
ducla <ducla@uiza.io>
2019-10-25 15:17:31 +0700

    looking for frame

--- ffmpeg/ngx_ffmpeg_module.c
+++ ffmpeg/ngx_ffmpeg_module.c
@@ -255,13 +255,13 @@ ngx_rtmp_ffmpeg_video(ngx_rtmp_session_t *s, ngx_rtmp_header_t *h,
     //     ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "ffmpeg: Can not alloc frame.");
     //     return NGX_ERROR;
     // }
-    // ret = avcodec_send_packet(ctx->out_av_codec_context, pkt);
-    // if(ret < 0){
-    //     ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "ffmpeg: Can not send packet to decoder %s \n", av_err2str(ret));
-    //     // return NGX_ERROR;
-    // }else{
-    //     ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "ffmpeg: decode success.");
-    // }
+    ret = avcodec_send_packet(ctx->out_av_codec_context, pkt);
+    if(ret < 0){
+        ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "ffmpeg: Can not send packet to decoder %s \n", av_err2str(ret));
+        // return NGX_ERROR;
+    }else{
+        ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "ffmpeg: decode success.");
+    }
     ret = av_interleaved_write_frame(ctx->out_av_format_context, pkt);
     if(ret < 0){
         ngx_log_error(NGX_LOG_ERR, s->connection->log, 0, "ffmpeg: Can not write data %s.", av_err2str(ret));
